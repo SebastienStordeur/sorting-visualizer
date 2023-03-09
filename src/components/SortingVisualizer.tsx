7d7193e445320ef20aabb0332695d99eb3d1bd1f
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-03-09 20:59:43 +0100

    Merge in progress

--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { randomNumberFromInterval, resetArray } from "../utils/Array/Array";
+import { mergeSort } from "../utils/Sorting/MergeSort";
 
 const SortingVisualizer: React.FC = () => {
-  const [array, setArray] = useState<number[]>();
+  const [array, setArray] = useState<number[]>(
+    Array.from({ length: 100 }, () => Math.floor(Math.random() * (300 - 5 + 1)))
+  );
 
+  const sortedArray = array.slice().sort((a, b) => a - b);
+  console.log("Expected Result", sortedArray);
+
+  //console.log(array);
+  //mergeSort(array, 0, array.length - 1);
   return (
     <>
       <div className="visualizer">
         {array &&
           array.map((number: number, index: number) => {
+            //console.log(array);
             return <div className="bar" style={{ height: number + "px" }} key={index}></div>;
           })}
       </div>
-      <button onClick={() => resetArray(150, setArray)}>Generate a new array</button>
+      <button onClick={() => resetArray(300, setArray)}>Generate a new array</button>
+      <button onClick={() => mergeSort(array)}>Sort</button>
     </>
   );
 };
--- src/utils/Sorting/MergeSort.ts
+++ src/utils/Sorting/MergeSort.ts
@@ -1,1 +1,54 @@
-export const mergeSort = () => {};
+export const mergeSort = (array: number[]) => {
+  //Merges 2 subarrays of array
+  //1st subarray is array[left index, mid]
+  //2nd subarray is array[mid + 1, end]
+
+  /** If the array is too short to be sorted */
+
+  const temporaryArray = array.slice();
+  mergeHelper(array, 0, array.length - 1, temporaryArray);
+};
+
+function mergeHelper(array: number[], startIndex: number, endIndex: number, temporaryArray: number[]) {
+  if (startIndex === endIndex) return;
+  const midIndex = Math.floor((startIndex + endIndex) / 2);
+
+  mergeHelper(temporaryArray, startIndex, midIndex, array);
+  mergeHelper(temporaryArray, midIndex + 1, endIndex, array);
+  merge(array, startIndex, midIndex, endIndex, temporaryArray);
+}
+
+function merge(array: number[], startIndex: number, midIndex: number, endIndex: number, temporaryArray: number[]) {
+  const n1 = midIndex - startIndex + 1;
+  const n2 = endIndex - midIndex;
+
+  //temporary arrays
+  let subarray1 = new Array(n1);
+  let subarray2 = new Array(n2);
+
+  for (var i = 0; i < n1; i++) subarray1[i] = array[startIndex + i];
+  for (var j = 0; j < n2; j++) subarray2[j] = array[midIndex + 1 + j];
+
+  console.log(subarray1, subarray2);
+
+  // let k = startIndex;
+  // let i = startIndex;
+  // let j = midIndex + 1;
+
+  // while (i <= midIndex && j <= endIndex) {
+  //   if (temporaryArray[i] < temporaryArray[j]) {
+  //     temporaryArray[k++] = array[i++];
+  //   } else {
+  //     temporaryArray[k++] = array[j++];
+  //   }
+  // }
+
+  // while (i <= midIndex) {
+  //   temporaryArray[k++] = array[i++];
+  // }
+
+  // while (j <= endIndex) {
+  //   temporaryArray[k++] = array[j++];
+  // }
+  // console.log(array, temporaryArray);
+}
