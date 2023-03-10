9c19761f84040256872d6104c726af672706e78c
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-03-10 21:21:41 +0100

    Removed unused variables and code

--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -1,6 +1,5 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { quickSort } from "../utils/Algorithms/QuickSort";
-import { resetArray } from "../utils/Array/Array";
 import { mergeSort } from "../utils/Sorting/MergeSort";
 import Bar from "./Bar";
 
@@ -11,8 +10,6 @@ interface VisualizerInterface {
 
 const SortingVisualizer: React.FC<VisualizerInterface> = ({ array, setArray }) => {
   const quickSortedArray = quickSort(array);
-  /* console.log(quickSortedArray); */
-  //const sortedArray = array.slice().sort((a, b) => a - b);
 
   const setSortedArray = async () => {
     const sortArray: any = await mergeSort(array, 0, array.length - 1);
@@ -27,7 +24,6 @@ const SortingVisualizer: React.FC<VisualizerInterface> = ({ array, setArray }) =
             return <Bar number={number} key={index} />;
           })}
       </div>
-      <button onClick={() => resetArray(100, setArray)}>Generate a new array</button>
       <button onClick={() => mergeSort(array, 0, array.length - 1)}>Sort</button>
       <button onClick={() => setSortedArray()}>Display sorted array</button>
 
